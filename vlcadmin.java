516ca082174aade58405be09391994a4c8f2a9bf
yogeshnuthan <yogeshecoford@gmail.com>
2026-04-27 00:21:42 +0530

    Update vlcadmin.java

--- vlcadmin.java
+++ vlcadmin.java
@@ -17,3 +17,18 @@ public class vlcadmin {
         mediaPlayer.mediaPlayer().media().play("sample.mp4");
     }
 }
+<project xmlns="http://maven.apache.org/POM/4.0.0">
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>example</groupId>
+  <artifactId>vlc-project</artifactId>
+  <version>1.0</version>
+
+  <dependencies>
+    <dependency>
+      <groupId>uk.co.caprica</groupId>
+      <artifactId>vlcj</artifactId>
+      <version>4.8.2</version>
+    </dependency>
+  </dependencies>
+</project>
