f9e35ece352822b3bdf588e08e310c7e4458010c
yogeshnuthan <yogeshecoford@gmail.com>
2026-04-27 00:25:46 +0530

    Update vlcadmin.java

--- vlcadmin.java
+++ vlcadmin.java
@@ -1,22 +1,3 @@
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
-
-import javax.swing.*;
-
-public class vlcadmin {
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Simple VLC Player");
-        EmbeddedMediaPlayerComponent mediaPlayer = new EmbeddedMediaPlayerComponent();
-
-        frame.setContentPane(mediaPlayer);
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        // Play video (change path)
-        mediaPlayer.mediaPlayer().media().play("sample.mp4");
-    }
-}
 <project xmlns="http://maven.apache.org/POM/4.0.0">
   <modelVersion>4.0.0</modelVersion>
 
